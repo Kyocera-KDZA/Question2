1a74d6ad493d4732bbfc451b0ac73624f922f9ae
ys <kdza.kyocera@gmail.com>
2022-12-08 17:04:52 +0000

    nav

--- app/src/main/java/com/itada/question2/MainActivity.kt
+++ app/src/main/java/com/itada/question2/MainActivity.kt
@@ -5,22 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
     lateinit var drawer: DrawerLayout
+    lateinit var navController: NavController
+    lateinit var appConfigBar: AppBarConfiguration
+   private lateinit var nav : NavigationView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //toolBar = findViewById(R.id.toolbar)
+       // setSupportActionBar(toolBar)
+
         drawer  = findViewById(R.id.drawerLayout)
-        val nav : NavigationView = findViewById(R.id.nav_view)
+        nav = findViewById(R.id.nav_view)
+       // navController = findViewById(R.id.content_frame)
 
         toggle = ActionBarDrawerToggle(this,drawer,R.string.open,R.string.close)
         drawer.addDrawerListener(toggle)
@@ -31,9 +42,14 @@ class MainActivity : AppCompatActivity() {
 
         nav.setNavigationItemSelectedListener {
 
+            it.isChecked = true
             when(it.itemId){
 
-                R.id.nav_home -> Toast.makeText(applicationContext,"Go Home",Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> replaceFrag(HomeFragment(), it.title.toString())
+                R.id.nav_frag1 -> replaceFrag(FirstFragment(), it.title.toString())
+                R.id.nav_frag2 -> replaceFrag(SecondFragment(), it.title.toString())
+                R.id.nav_frag3 -> replaceFrag(ThirdFragment(), it.title.toString())
+
             }
 
             true
@@ -41,6 +57,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun replaceFrag(frag: Fragment, title: String)
+    {
+        val fragManager = supportFragmentManager
+        val fragTrans = fragManager.beginTransaction()
+        fragTrans.replace(R.id.content_frame, frag)
+        setTitle(title)
+        fragTrans.commit()
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if(toggle.onOptionsItemSelected(item)){
