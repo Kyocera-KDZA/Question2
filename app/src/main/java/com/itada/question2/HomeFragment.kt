83c02893eb15334e43be129c6ddfc09eb32d30b8
ys <kdza.kyocera@gmail.com>
2022-12-08 17:26:43 +0000

    we

--- app/src/main/java/com/itada/question2/HomeFragment.kt
+++ app/src/main/java/com/itada/question2/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Spinner
+import androidx.navigation.findNavController
 
 
 class HomeFragment : Fragment() {
@@ -14,7 +17,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home,container, false)
+
+        val rate = view.findViewById<Spinner>(R.id.spinner)
+        val rateBtn = view.findViewById<Button>(R.id.btn_home)
+
+        rateBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_rateActivity)
+        }
+
+        return view
+
     }
 
 }
